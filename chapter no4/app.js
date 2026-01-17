2bc98695a1a8b12684b5b3e8638fa2d8c90d1dd5
Zafir kirmani <zafirkirmani826@gmail.com>
2026-01-17 21:20:40 +0500

    Add files via upload

---
+++ chapter no4/app.js
@@ -0,0 +1,18 @@
+document.title="chapter no 4";
+var x = 1, y = 2, z = 3;
+
+
+// legal variables 
+let name;
+let age1;
+let _total;
+let $price;
+let userName;
+
+// illeagal variable
+
+let 1name;      // Cannot start with number
+let user name; // Space not allowed
+let total-amt; // Hyphen not allowed
+let @price;    // Special symbol not allowed
+let var;       // Reserved keyword
