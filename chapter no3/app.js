24afbdcfa595beb74733370e47bca482fbde60f8
Zafir kirmani <zafirkirmani826@gmail.com>
2026-01-17 21:20:14 +0500

    Add files via upload

---
+++ chapter no3/app.js
@@ -0,0 +1,43 @@
+
+    //! declare a variable called age and assign it to your age show your age in alert
+    //box
+
+    var age;
+age="17 years old";
+alert("I am"+ age);
+    
+//  declare and intialize a varibleto keep track of how many times a visitor has 
+    // visited a web pageshow his/hernumber of visits on your web page for example
+    // your have visited this site 'n'times
+
+var visit = 14;
+alert("You have visited this site"+visit+"times");
+   
+
+// declare a variable called age and assign it to your age show your age in alert
+    // box
+var age;
+
+age="17 years old";
+alert("I am"+ age);
+
+// declare a variable called birth year and assign to your birthday year 
+// show the following message in your browers
+
+var birthdayyear = 2009;
+document.getElementById("text").innerHTML ="My  birthday year is" + " " +birthdayyear;
+
+// A visitor an online  clothing store www.xyz.clothing.com.write  a script 
+// to store in variables the following example
+// 1)visitor name 
+// 2)product titile
+// 3)quantitiy 
+// show the following message that john john doe ordered 5 shirts 
+// on xyz clothing store  
+
+ var visitorname = "johndoe";
+ var producttitle = "shirts";
+ var quantity = 5 ;
+
+document.getElementById("deal").innerHTML= visitorname +" "+ "ordered" +" "+quantity +" " + producttitle +" "+ "from xyzclothingstore";
+
